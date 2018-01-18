513de4c4d6a7bc3bdb9a81aee7109d023496e1a8
Steve Desmond <steve@vtsv.ca>
2018-01-18 15:12:37 -0500

    switch to MS syndication feed writer

--- Web/Controllers/FeedsController.cs
+++ Web/Controllers/FeedsController.cs
@@ -1,7 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
 using System.Xml.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.SyndicationFeed;
+using Microsoft.SyndicationFeed.Atom;
+using Microsoft.SyndicationFeed.Rss;
 using Web.Models;
 
 namespace Web.Controllers
@@ -15,29 +23,60 @@ namespace Web.Controllers
             livePosts = Cache.Pages.Where(p => p.Category == "Blog" && p.Crawl).OrderByDescending(p => p.Timestamp);
         }
 
-        public IActionResult rss()
+        public async Task<IActionResult> rss()
         {
-            var xml = new XElement("rss",
-                new XAttribute("version", "2.0"),
-                new XElement("channel",
-                    new XElement("title", Settings.Title),
-                    new XElement("description", Settings.Description),
-                    new XElement("link", Settings.Domain),
-                    livePosts.Select(p => new XElement("item",
-                            new XElement("title", p.Title),
-                            new XElement("description", p.Description),
-                            new XElement("link", p.FullURL),
-                            new XElement("guid", p.FullURL),
-                            new XElement("pubDate", p.Timestamp.ToString("r"))
-                        )
-                    )
-                )
-            );
-            return new ContentResult { Content = xml.ToString(), ContentType = "application/rss+xml" };
+            var stringWriter = new StringWriter();
+            using (var xmlWriter = XmlWriter.Create(stringWriter, new XmlWriterSettings { Async = true, Indent = true, Encoding = Encoding.UTF8, WriteEndDocumentOnClose = true }))
+            {
+                var rssWriter = new RssFeedWriter(xmlWriter);
+                var tasks = new List<Task>
+                {
+                    rssWriter.WriteTitle(Settings.Title),
+                    rssWriter.WriteDescription(Settings.Description),
+                    rssWriter.Write(new SyndicationLink(new Uri(Settings.Domain)))
+                };
+                tasks.AddRange(livePosts.Select(post => rssWriter.Write(SyndicationItem(post))));
+                Task.WaitAll(tasks.ToArray());
+                await rssWriter.Flush();
+            }
+
+            return new ContentResult { Content = stringWriter.ToString(), ContentType = "application/rss+xml" };
         }
 
-        public IActionResult atom()
+        private static SyndicationItem SyndicationItem(Page p)
         {
+            var post = new SyndicationItem
+            {
+                Id = p.FullURL,
+                Title = p.Title,
+                Description = p.Description,
+                Published = p.Timestamp.ToLocalTime(),
+                LastUpdated = p.Timestamp.ToLocalTime()
+            };
+            post.AddLink(new SyndicationLink(new Uri(p.FullURL)));
+            post.AddContributor(new SyndicationPerson("Steve Desmond", "steve@stevedesmond.ca"));
+            return post;
+        }
+
+        public async Task<IActionResult> atom()
+        {
+            var stringWriter = new StringWriter();
+            using (var xmlWriter = XmlWriter.Create(stringWriter, new XmlWriterSettings { Async = true, Indent = true, Encoding = Encoding.UTF8, WriteEndDocumentOnClose = true }))
+            {
+                var atomWriter = new AtomFeedWriter(xmlWriter);
+                var tasks = new List<Task>
+                {
+                    atomWriter.WriteTitle(Settings.Title),
+                    atomWriter.WriteSubtitle(Settings.Description),
+                    atomWriter.WriteId(Settings.Domain),
+                    atomWriter.Write(new SyndicationLink(new Uri(Settings.Domain))),
+                    atomWriter.WriteUpdated(livePosts.First().Timestamp.ToLocalTime())
+                };
+                tasks.AddRange(livePosts.Select(post => atomWriter.Write(SyndicationItem(post))));
+                Task.WaitAll(tasks.ToArray());
+                await atomWriter.Flush();
+            }
+
             var xml = new XElement("feed",
                 new XElement("title", Settings.Title),
                 new XElement("subtitle", Settings.Description),
@@ -58,6 +97,8 @@ namespace Web.Controllers
             );
             foreach (var element in xml.DescendantsAndSelf())
                 element.Name = XName.Get(element.Name.LocalName, "http://www.w3.org/2005/Atom");
+            
+            return new ContentResult { Content = stringWriter.ToString(), ContentType = "application/rss+xml" };
             return new ContentResult { Content = xml.ToString(), ContentType = "application/atom+xml" };
         }
     }
