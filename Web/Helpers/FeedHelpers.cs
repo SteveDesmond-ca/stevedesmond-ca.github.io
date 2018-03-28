ffaff6b4e46c6ef96bf54f3d34e69e49ff7b66e1
Steve Desmond <steve@vtsv.ca>
2018-03-28 14:18:36 -0400

    force UTF-8 for feeds

--- Web/Helpers/FeedHelpers.cs
+++ Web/Helpers/FeedHelpers.cs
@@ -18,60 +18,59 @@ namespace Web.Helpers
         {
             using (var stringWriter = new StringWriter())
             {
-                using (var xmlWriter = XmlWriter.Create(stringWriter,
-                    new XmlWriterSettings
-                    {
-                        Async = true,
-                        Indent = true,
-                        Encoding = Encoding.UTF8,
-                        WriteEndDocumentOnClose = true
-                    }))
+                var memoryStream = new MemoryStream();
+                var streamWriter = new StreamWriter(memoryStream, Encoding.UTF8);
+                var xmlWriterSettings = new XmlWriterSettings
                 {
-                    var rssWriter = new RssFeedWriter(xmlWriter);
-                    var tasks = new List<Task>
-                    {
-                        rssWriter.WriteTitle(Settings.Title),
-                        rssWriter.WriteDescription(Settings.Description),
-                        rssWriter.Write(new SyndicationLink(new Uri(Settings.Domain)))
-                    };
-                    tasks.AddRange(pages.Select(p => rssWriter.Write(p.ToSyndicationItem())));
-                    Task.WaitAll(tasks.ToArray());
-                    await rssWriter.Flush();
-                }
+                    Async = true,
+                    Indent = true,
+                    Encoding = Encoding.UTF8,
+                    WriteEndDocumentOnClose = true
+                };
 
-                return stringWriter.ToString();
+                var xmlWriter = XmlWriter.Create(streamWriter, xmlWriterSettings);
+                var rssWriter = new RssFeedWriter(xmlWriter);
+                var tasks = new List<Task>
+                {
+                    rssWriter.WriteTitle(Settings.Title),
+                    rssWriter.WriteDescription(Settings.Description),
+                    rssWriter.Write(new SyndicationLink(new Uri(Settings.Domain)))
+                };
+                tasks.AddRange(pages.Select(p => rssWriter.Write(p.ToSyndicationItem())));
+                Task.WaitAll(tasks.ToArray());
+                await rssWriter.Flush();
+
+                return Encoding.UTF8.GetString(memoryStream.GetBuffer());
             }
         }
 
         public static async Task<string> ToAtom(this IList<Page> pages)
         {
-            using (var stringWriter = new StringWriter())
+            var memoryStream = new MemoryStream();
+            var streamWriter = new StreamWriter(memoryStream, Encoding.UTF8);
+            var xmlWriterSettings = new XmlWriterSettings
             {
-                using (var xmlWriter = XmlWriter.Create(stringWriter,
-                    new XmlWriterSettings
-                    {
-                        Async = true,
-                        Indent = true,
-                        Encoding = Encoding.UTF8,
-                        WriteEndDocumentOnClose = true
-                    }))
-                {
-                    var atomWriter = new AtomFeedWriter(xmlWriter);
-                    var tasks = new List<Task>
-                    {
-                        atomWriter.WriteTitle(Settings.Title),
-                        atomWriter.WriteSubtitle(Settings.Description),
-                        atomWriter.WriteId(Settings.Domain),
-                        atomWriter.Write(new SyndicationLink(new Uri(Settings.Domain))),
-                        atomWriter.WriteUpdated(pages.First().Timestamp.ToLocalTime())
-                    };
-                    tasks.AddRange(pages.Select(p => atomWriter.Write(p.ToSyndicationItem())));
-                    Task.WaitAll(tasks.ToArray());
-                    await atomWriter.Flush();
-                }
+                Async = true,
+                Indent = true,
+                Encoding = Encoding.UTF8,
+                WriteEndDocumentOnClose = true
+            };
 
-                return stringWriter.ToString();
-            }
+            var xmlWriter = XmlWriter.Create(streamWriter, xmlWriterSettings);
+            var atomWriter = new AtomFeedWriter(xmlWriter);
+            var tasks = new List<Task>
+            {
+                atomWriter.WriteTitle(Settings.Title),
+                atomWriter.WriteSubtitle(Settings.Description),
+                atomWriter.WriteId(Settings.Domain),
+                atomWriter.Write(new SyndicationLink(new Uri(Settings.Domain))),
+                atomWriter.WriteUpdated(pages.First().Timestamp.ToLocalTime())
+            };
+            tasks.AddRange(pages.Select(p => atomWriter.Write(p.ToSyndicationItem())));
+            Task.WaitAll(tasks.ToArray());
+            await atomWriter.Flush();
+
+            return Encoding.UTF8.GetString(memoryStream.GetBuffer());
         }
 
         private static SyndicationItem ToSyndicationItem(this Page p)
